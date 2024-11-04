0a79d51bece7582d7a8eb9fa172cd8c5fed9bbe5
NathanIstace <144009062+fishingcactus-nathan-istace@users.noreply.github.com>
2024-11-04 16:40:20 +0100

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,7 @@
 def call() {
-  echo "${env.PROJECT_NAME} ${params.TEST_PARAM2}"
+  if (params.TEST_PARAM2 == null){
+    echo "${env.PROJECT_NAME} has no param named TEST_PARAM2"
+  } else {
+    echo "${env.PROJECT_NAME} ${params.TEST_PARAM2}"
+  } 
 }
