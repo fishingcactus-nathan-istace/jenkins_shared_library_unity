c268b346243d0e1d26897ecf848415fefc7e0767
NathanIstace <144009062+fishingcactus-nathan-istace@users.noreply.github.com>
2024-11-04 16:49:24 +0100

    Update checkParameters.groovy

--- vars/checkParameters.groovy
+++ vars/checkParameters.groovy
@@ -4,6 +4,7 @@ def call() {
           if (params.ROOT_ARCHIVE_DIRECTORY == null || params.ROOT_ARCHIVE_DIRECTORY == "") {
               error "ROOT_ARCHIVE_DIRECTORY must be set !";
           }
+          echo "params.ROOT_ARCHIVE_DIRECTORY = ${params.ROOT_ARCHIVE_DIRECTORY}";
       } catch (Exception err) {
           echo  "ROOT_ARCHIVE_DIRECTORY must be set !";
           currentBuild.result = "FAILURE";
