e3651ca6f102bba27ebf8df8d3227eb790c4d699
NathanIstace <144009062+fishingcactus-nathan-istace@users.noreply.github.com>
2024-11-05 10:33:29 +0100

    Update commitSha.groovy

--- vars/commitSha.groovy
+++ vars/commitSha.groovy
@@ -2,7 +2,7 @@ def writeToCsFile( String csFilePath ) {
   def date = new Date((long)currentBuild.startTimeInMillis).format("MM/dd/yyyy HH:mm:ss a") ;
   def sha = get();
   
-  def text = readFile(file: fileName);
+  def text = readFile(file: csFilePath);
   def commitText = text.replaceAll("(?s)commitSHA = \".*\"", "commitSHA = \"${sha} ${date}\"");
   
   writeFile( file: csFilePath, text: commitText );
