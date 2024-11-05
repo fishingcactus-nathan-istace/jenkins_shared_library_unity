ffb7211e0146432cc41f2b47024fc822058d29ae
NathanIstace <144009062+fishingcactus-nathan-istace@users.noreply.github.com>
2024-11-05 08:23:42 +0100

    Delete vars/test.groovy

--- vars/test.groovy
+++
@@ -1,7 +0,0 @@
-def call() {
-  if (params.TEST_PARAM2 == null){
-    echo "${env.PROJECT_NAME} has no param named TEST_PARAM2"
-  } else {
-    echo "${env.PROJECT_NAME} ${params.TEST_PARAM2}"
-  } 
-}
